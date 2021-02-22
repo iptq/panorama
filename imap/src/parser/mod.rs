875041edfd9b9a43880161b44c65fa311065acae
Michael Zhang <mail@mzhang.io>
2021-02-22 16:32:00 -0600

    add rfc3501 rules that relate to response parsing

--- imap/src/parser/mod.rs
+++ imap/src/parser/mod.rs
@@ -14,10 +14,10 @@ pub fn parse_capability(s: &str) -> Result<Capability, Error<Rule>> {
             let mut inner = pair.into_inner();
             let pair = inner.next().unwrap();
             match pair.as_rule() {
-                Rule::auth_type => Capability::Auth(pair.as_str().to_owned()),
+                Rule::auth_type => Capability::Auth(pair.as_str().to_uppercase().to_owned()),
                 Rule::atom => match pair.as_str() {
                     "IMAP4rev1" => Capability::Imap4rev1,
-                    s => Capability::Atom(s.to_owned()),
+                    s => Capability::Atom(s.to_uppercase().to_owned()),
                 },
                 _ => unreachable!("{:?}", pair),
             }
@@ -25,6 +25,10 @@ pub fn parse_capability(s: &str) -> Result<Capability, Error<Rule>> {
         _ => unreachable!("{:?}", pair),
     };
     Ok(cap)
+}
+
+pub fn parse_response(s: &str) -> Result<Response, Error<Rule>> {
+    todo!()
 }
 
 #[cfg(test)]
@@ -39,6 +43,7 @@ mod tests {
         assert_eq!(parse_capability("IMAP4rev1"), Ok(Capability::Imap4rev1));
         assert_eq!(parse_capability("LOGINDISABLED"), Ok(Capability::Atom("LOGINDISABLED".to_owned())));
         assert_eq!(parse_capability("AUTH=PLAIN"), Ok(Capability::Auth("PLAIN".to_owned())));
+        assert_eq!(parse_capability("auth=plain"), Ok(Capability::Auth("PLAIN".to_owned())));
 
         assert!(parse_capability("(OSU)").is_err());
         assert!(parse_capability("\x01HELLO").is_err());
