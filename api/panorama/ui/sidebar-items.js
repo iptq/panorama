fac18abb983a66ffd7b9621efcf4d4f47b9e2051
iptq <iptq@users.noreply.github.com>
2021-02-27 06:30:21 +0000

    Deploying to gh-pages from @ iptq/panorama@2074b95778e98ca2a4290855b5c5af83cef51e16 🚀

--- api/panorama/ui/sidebar-items.js
+++ api/panorama/ui/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["FRAME_DURATION",""]],"fn":[["run_ui","UI entrypoint."]],"mod":[["drawable",""],["table",""],["tabs",""]],"struct":[["Rect","X Y W H"]],"type":[["Screen","Type alias for the screen object we're drawing to"]]});
+initSidebarItems({"constant":[["FRAME_DURATION",""]],"fn":[["run_ui","UI entrypoint."]],"mod":[["table",""],["tabs",""],["widget",""]],"struct":[["Rect","X Y W H"]],"type":[["Screen","Type alias for the screen object we're drawing to"]]});
