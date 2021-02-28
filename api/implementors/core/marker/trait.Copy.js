f728a16c65fcd7ae905160ad34a6afaada1c5d9d
iptq <iptq@users.noreply.github.com>
2021-02-28 04:17:51 +0000

    Deploying to gh-pages from @ iptq/panorama@631fdd8b737c449303332dd183db479dec51cf1b 🚀

--- api/implementors/core/marker/trait.Copy.js
+++ api/implementors/core/marker/trait.Copy.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["panorama"] = [{"text":"impl Copy for Rect","synthetic":false,"types":[]}];
+implementors["panorama"] = [{"text":"impl Copy for Cell","synthetic":false,"types":[]},{"text":"impl Copy for Rect","synthetic":false,"types":[]}];
 implementors["panorama_imap"] = [{"text":"impl Copy for Rule","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
