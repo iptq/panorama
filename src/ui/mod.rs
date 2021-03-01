84cd7ae0f4598d13807819817c4339821eaa7228
Michael Zhang <mail@mzhang.io>
2021-03-01 03:35:14 -0600

    playing around with ui

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -14,6 +14,7 @@ use tokio::{sync::mpsc, time};
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
     text::Spans,
     widgets::*,
     Terminal,
@@ -34,23 +35,47 @@ pub async fn run_ui(mut stdout: Stdout, exit_tx: mpsc::Sender<()>) -> Result<()>
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(0)
-                .constraints(
-                    [
-                        Constraint::Percentage(10),
-                        Constraint::Percentage(80),
-                        Constraint::Percentage(10),
-                    ]
-                    .as_ref(),
-                )
+                .constraints([
+                    Constraint::Length(1),
+                    Constraint::Max(5000),
+                    // Constraint::Percentage(10),
+                    // Constraint::Percentage(80),
+                    // Constraint::Percentage(10),
+                ])
                 .split(f.size());
-            let block = Block::default().title("Block").borders(Borders::NONE);
-            f.render_widget(block, chunks[0]);
-            let block = Block::default().title("Block 2").borders(Borders::ALL);
-            f.render_widget(block, chunks[1]);
 
+            let chunks2 = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(0)
+                .constraints([
+                    Constraint::Length(20),
+                    Constraint::Max(5000),
+                    //
+                ])
+                .split(chunks[1]);
+
+            // this is the title bar
             let titles = vec!["hellosu"].into_iter().map(Spans::from).collect();
             let tabs = Tabs::new(titles);
-            f.render_widget(tabs, chunks[2]);
+            f.render_widget(tabs, chunks[0]);
+
+            // TODO: check active tab
+            let items = [
+                ListItem::new("Osu"),
+                ListItem::new("Game").style(Style::default().add_modifier(Modifier::BOLD)),
+            ];
+            let dirlist = List::new(items)
+                .block(Block::default().title("List").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+                .highlight_symbol(">>");
+            f.render_widget(dirlist, chunks2[0]);
+
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            f.render_widget(block, chunks2[1]);
+
+            // let block = Block::default().title("Block 2").borders(Borders::ALL);
+            // f.render_widget(block, chunks[1]);
         })?;
 
         let event = if event::poll(FRAME_DURATION)? {
