f8ab662c1e9215c60c0f83d2140777e595e3d6f1
iptq <iptq@users.noreply.github.com>
2021-03-02 22:52:17 +0000

    Deploying to gh-pages from @ iptq/panorama@3f726f3129a496181e2e467cf47d72a14b916ad9 🚀

--- api/panorama_imap/parser/literal/sidebar-items.js
+++ api/panorama_imap/parser/literal/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["literal_internal",""],["noop",""]],"type":[["PSR",""]]});
+initSidebarItems({"fn":[["literal_internal","This is a hack around the literal syntax to allow us to parse characters statefully."],["noop",""]],"type":[["PSR",""]]});
