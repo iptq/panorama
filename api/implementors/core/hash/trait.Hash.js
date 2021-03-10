1fd79267ae06bd1dc6d3af6d2c86cc6808278989
iptq <iptq@users.noreply.github.com>
2021-03-10 11:12:54 +0000

    Deploying to gh-pages from @ iptq/panorama@4e3d2e63f368f38d86c4f7cfdc593ca42d024d89 🚀

--- api/implementors/core/hash/trait.Hash.js
+++ api/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["panorama_imap"] = [{"text":"impl Hash for Rule","synthetic":false,"types":[]},{"text":"impl Hash for Capability","synthetic":false,"types":[]}];
+implementors["panorama_imap"] = [{"text":"impl Hash for Rule","synthetic":false,"types":[]},{"text":"impl Hash for Capability","synthetic":false,"types":[]},{"text":"impl Hash for MailboxFlag","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
