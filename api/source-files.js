89df20c277a5cbac065ea8cc5b05e7e47145ecc4
iptq <iptq@users.noreply.github.com>
2021-03-20 08:28:08 +0000

    Deploying to gh-pages from @ iptq/panorama@680c9c9ebc3a38c45d3ab5d5da77888465c6ee56 🚀

--- api/source-files.js
+++ api/source-files.js
@@ -1,5 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["panorama"] = {"name":"","dirs":[{"name":"mail","files":["client.rs","metadata.rs","mod.rs"]},{"name":"ui","files":["colon_prompt.rs","input.rs","keybinds.rs","mail_view.rs","messages.rs","mod.rs","windows.rs"]}],"files":["config.rs","lib.rs","script.rs"]};
 sourcesIndex["panorama_imap"] = {"name":"","dirs":[{"name":"client","files":["auth.rs","inner.rs","mod.rs"]},{"name":"parser","files":["literal.rs","mod.rs"]}],"files":["codec.rs","command.rs","lib.rs","response.rs"]};
+sourcesIndex["panorama_maildir"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["panorama_smtp"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
