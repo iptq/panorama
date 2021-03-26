8899950f4642d8789d608ec083bc776e670f6bb0
iptq <iptq@users.noreply.github.com>
2021-03-26 19:47:13 +0000

    Deploying to gh-pages from @ iptq/panorama@22f11544e0135561acb6923e1053755b4680ff26 🚀

--- api/panorama_tui/sidebar-items.js
+++ api/panorama_tui/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"enum":[["ResizeBehavior",""]],"fn":[["color_conv",""]],"struct":[["Frame",""],["ModifierDiff",""],["Terminal",""],["TerminalOptions","Options to pass to [`Terminal::with_options`]"],["Viewport",""]]});
